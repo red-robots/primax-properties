cba9c3950508edb32a85aff72194af6d2909cdea
Austin Crane <austincrane@gmail.com>
2016-05-02 16:07:38 -0400

    more map

--- page-templates/page-map.php
+++ page-templates/page-map.php
@@ -4,11 +4,13 @@
  */
 
 get_header(); ?>
-
+<div class="wrapper">
 	<div id="primary" class="">
 		<main id="main" class="site-main" role="main">
 
-			
+			<header class="entry-header">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</header><!-- .entry-header -->
 <?php
 	
 	$listArray = array();
@@ -17,7 +19,9 @@ get_header(); ?>
 	$wp_query = new WP_Query();
 	$wp_query->query(array(
 		'post_type'=>'market',
-		'posts_per_page' => -1
+		'posts_per_page' => -1, 
+		'order' => 'ASC', 
+		'orderby' => 'title'
 	));
 	if ($wp_query->have_posts()) : ?>
 	
@@ -35,57 +39,57 @@ get_header(); ?>
     }
 
     // Set the states variables
-    if($title == 'Alabama') {$dataState = 'al';$letter = 'A';
-    } elseif($title == 'Alaska') {$dataState = 'ak';$letter = 'B';
-	} elseif($title == 'Arkansas') {$dataState = 'ar';$letter = 'C';
-	} elseif($title == 'Arizona') {$dataState = 'az';$letter = 'D';
-	} elseif($title == 'California') {$dataState = 'ca';$letter = 'E';
-	} elseif($title == 'Colorado') {$dataState = 'co';$letter = 'F';
-	} elseif($title == 'Connecticut') {$dataState = 'ct';$letter = 'G';
-	} elseif($title == 'Delaware') {$dataState = 'de';$letter = 'H';
-	} elseif($title == 'DC') {$dataState = 'dc';$letter = 'I';
-	} elseif($title == 'Florida') {$dataState = 'fl';$letter = 'J';
-	} elseif($title == 'Georgia') {$dataState = 'ga';$letter = 'K';
-	} elseif($title == 'Hiawai') {$dataState = 'hi';$letter = 'L';
-	} elseif($title == 'Idaho') {$dataState = 'id';$letter = 'M';
-	} elseif($title == 'Illinois') {$dataState = 'il';$letter = 'N';
-	} elseif($title == 'Indiana') {$dataState = 'in';$letter = 'O';
-	} elseif($title == 'Iowa') {$dataState = 'ia';$letter = 'P';
-	} elseif($title == 'Kansas') {$dataState = 'ks';$letter = 'Q';
-	} elseif($title == 'Kentucky') {$dataState = 'ky';$letter = 'R';
-	} elseif($title == 'Louisiana') {$dataState = 'la';$letter = 'S';
-	} elseif($title == 'Maine') {$dataState = 'me';$letter = 'T';
-	} elseif($title == 'Maryland') {$dataState = 'md';$letter = 'U';
-	} elseif($title == 'Massachusetts') {$dataState = 'ma';$letter = 'V';
-	} elseif($title == 'Michigan') {$dataState = 'mi';$letter = 'W';
-	} elseif($title == 'Minnesota') {$dataState = 'mn';$letter = 'X';
-	} elseif($title == 'Mississippi') {$dataState = 'ms';$letter = 'Y';
-	} elseif($title == 'Missouri') {$dataState = 'mo';$letter = 'Z';
-	} elseif($title == 'Montana') {$dataState = 'mt';$letter = 'a';
-    } elseif($title == 'Nebraska') {$dataState = 'ne';$letter = 'b';
-	} elseif($title == 'Nevada') {$dataState = 'nv';$letter = 'c';
-	} elseif($title == 'New Hampshire') {$dataState = 'nh';$letter = 'd';
-	} elseif($title == 'New Jersey') {$dataState = 'nj';$letter = 'e';
-	} elseif($title == 'New Mexico') {$dataState = 'nm';$letter = 'f';
-	} elseif($title == 'New York') {$dataState = 'ny';$letter = 'g';
-	} elseif($title == 'North Carolina') {$dataState = 'nc';$letter = 'h';
-	} elseif($title == 'North Dakota') {$dataState = 'nd';$letter = 'i';
-	} elseif($title == 'Ohio') {$dataState = 'oh';$letter = 'j';
-	} elseif($title == 'Oklahoma') {$dataState = 'ok';$letter = 'k';
-	} elseif($title == 'Oregon') {$dataState = 'or';$letter = 'l';
-	} elseif($title == 'Pennsylvania') {$dataState = 'pa';$letter = 'm';
-	} elseif($title == 'Rhode Island') {$dataState = 'ri';$letter = 'n';
-	} elseif($title == 'South Carolina') {$dataState = 'sc';$letter = 'o';
-    } elseif($title == 'South Dakota') {$dataState = 'sd';$letter = 'p';
-	} elseif($title == 'Tennesse') {$dataState = 'tn';$letter = 'q';
-	} elseif($title == 'Texas') {$dataState = 'tx';$letter = 'r';
-	} elseif($title == 'Utah') {$dataState = 'ut';$letter = 's';
-	} elseif($title == 'Virginia') {$dataState = 'va';$letter = 't';
-	} elseif($title == 'Vermont') {$dataState = 'vt';$letter = 'u';
-	} elseif($title == 'Washington') {$dataState = 'wa';$letter = 'v';
-	} elseif($title == 'West Virginia') {$dataState = 'wv';$letter = 'w';
-	} elseif($title == 'Wisconson') {$dataState = 'wi';$letter = 'x';
-	} elseif($title == 'Wyoming') {$dataState = 'wy';$letter = 'y';
+    if($title == 'Alabama') {$sTitle = 'Alabama'; $size = 'large-count'; $dataState = 'al'; $letter = 'A';
+    } elseif($title == 'Alaska') {$sTitle = 'Alaska'; $size = 'large-count'; $dataState = 'ak';$letter = 'B';
+	} elseif($title == 'Arkansas') {$sTitle = 'Arkansas'; $size = 'large-count'; $dataState = 'ar';$letter = 'C';
+	} elseif($title == 'Arizona') {$sTitle = 'Arizona'; $size = 'large-count'; $dataState = 'az';$letter = 'D';
+	} elseif($title == 'California') {$sTitle = 'California'; $size = 'large-count'; $dataState = 'ca';$letter = 'E';
+	} elseif($title == 'Colorado') {$sTitle = 'Colorado'; $size = 'large-count'; $dataState = 'co';$letter = 'F';
+	} elseif($title == 'Connecticut') {$sTitle = 'Connecticut'; $size = 'small-count'; $dataState = 'ct';$letter = 'G';
+	} elseif($title == 'Delaware') {$sTitle = 'Delaware'; $size = 'small-count'; $dataState = 'de';$letter = 'H';
+	} elseif($title == 'DC') {$sTitle = 'DC'; $size = 'small-count'; $dataState = 'dc';$letter = 'I';
+	} elseif($title == 'Florida') {$sTitle = 'Florida'; $size = 'large-count'; $dataState = 'fl';$letter = 'J';
+	} elseif($title == 'Georgia') {$sTitle = 'Georgia'; $size = 'large-count'; $dataState = 'ga';$letter = 'K';
+	} elseif($title == 'Hiawai') {$sTitle = 'Hiawai'; $size = 'large-count'; $dataState = 'hi';$letter = 'L';
+	} elseif($title == 'Idaho') {$sTitle = 'Idaho'; $size = 'large-count'; $dataState = 'id';$letter = 'M';
+	} elseif($title == 'Illinois') {$sTitle = 'Illinois'; $size = 'large-count'; $dataState = 'il';$letter = 'N';
+	} elseif($title == 'Indiana') {$sTitle = 'Indiana'; $size = 'large-count'; $dataState = 'in';$letter = 'O';
+	} elseif($title == 'Iowa') {$sTitle = 'Iowa'; $size = 'large-count'; $dataState = 'ia';$letter = 'P';
+	} elseif($title == 'Kansas') {$sTitle = 'Kansas'; $size = 'large-count'; $dataState = 'ks';$letter = 'Q';
+	} elseif($title == 'Kentucky') {$sTitle = 'Kentucky'; $size = 'large-count'; $dataState = 'ky';$letter = 'R';
+	} elseif($title == 'Louisiana') {$sTitle = 'Louisiana'; $size = 'large-count'; $dataState = 'la';$letter = 'S';
+	} elseif($title == 'Maine') {$sTitle = 'Maine'; $size = 'large-count'; $dataState = 'me';$letter = 'T';
+	} elseif($title == 'Maryland') {$sTitle = 'Maryland'; $size = 'small-count'; $dataState = 'md';$letter = 'U';
+	} elseif($title == 'Massachusetts') {$sTitle = 'Massachusetts'; $size = 'small-count'; $dataState = 'ma';$letter = 'V';
+	} elseif($title == 'Michigan') {$sTitle = 'Michigan'; $size = 'large-count'; $dataState = 'mi';$letter = 'W';
+	} elseif($title == 'Minnesota') {$sTitle = 'Minnesota'; $size = 'large-count'; $dataState = 'mn';$letter = 'X';
+	} elseif($title == 'Mississippi') {$sTitle = 'Mississippi'; $size = 'large-count'; $dataState = 'ms';$letter = 'Y';
+	} elseif($title == 'Missouri') {$sTitle = 'Missouri'; $size = 'large-count'; $dataState = 'mo';$letter = 'Z';
+	} elseif($title == 'Montana') {$sTitle = 'Montana'; $size = 'large-count'; $dataState = 'mt';$letter = 'a';
+    } elseif($title == 'Nebraska') {$sTitle = 'Nebraska'; $size = 'large-count'; $dataState = 'ne';$letter = 'b';
+	} elseif($title == 'Nevada') {$sTitle = 'Nevada'; $size = 'large-count'; $dataState = 'nv';$letter = 'c';
+	} elseif($title == 'New Hampshire') {$sTitle = 'New Hampshire'; $size = 'small-count'; $dataState = 'nh';$letter = 'd';
+	} elseif($title == 'New Jersey') {$sTitle = 'New Jersey'; $size = 'small-count'; $dataState = 'nj';$letter = 'e';
+	} elseif($title == 'New Mexico') {$sTitle = 'New Mexico'; $size = 'large-count'; $dataState = 'nm';$letter = 'f';
+	} elseif($title == 'New York') {$sTitle = 'New York'; $size = 'large-count'; $dataState = 'ny';$letter = 'g';
+	} elseif($title == 'North Carolina') {$sTitle = 'North Carolina'; $size = 'large-count'; $dataState = 'nc';$letter = 'h';
+	} elseif($title == 'North Dakota') {$sTitle = 'North Dakota'; $size = 'large-count'; $dataState = 'nd';$letter = 'i';
+	} elseif($title == 'Ohio') {$sTitle = 'Ohio'; $size = 'large-count'; $dataState = 'oh';$letter = 'j';
+	} elseif($title == 'Oklahoma') {$sTitle = 'Oklahoma'; $size = 'large-count'; $dataState = 'ok';$letter = 'k';
+	} elseif($title == 'Oregon') {$sTitle = 'Oregon'; $size = 'large-count'; $dataState = 'or';$letter = 'l';
+	} elseif($title == 'Pennsylvania') {$sTitle = 'Pennsylvania'; $size = 'large-count'; $dataState = 'pa';$letter = 'm';
+	} elseif($title == 'Rhode Island') {$sTitle = 'Rhode Island'; $size = 'small-count'; $dataState = 'ri';$letter = 'n';
+	} elseif($title == 'South Carolina') {$sTitle = 'South Carolina'; $size = 'large-count'; $dataState = 'sc';$letter = 'o';
+    } elseif($title == 'South Dakota') {$sTitle = 'South Dakota'; $size = 'large-count'; $dataState = 'sd';$letter = 'p';
+	} elseif($title == 'Tennesse') {$sTitle = 'Tennesse'; $size = 'large-count'; $dataState = 'tn';$letter = 'q';
+	} elseif($title == 'Texas') {$sTitle = 'Texas'; $size = 'large-count'; $dataState = 'tx';$letter = 'r';
+	} elseif($title == 'Utah') {$sTitle = 'Utah'; $size = 'large-count'; $dataState = 'ut';$letter = 's';
+	} elseif($title == 'Virginia') {$sTitle = 'Virginia'; $size = 'large-count'; $dataState = 'va';$letter = 't';
+	} elseif($title == 'Vermont') {$sTitle = 'Vermont'; $size = 'small-count'; $dataState = 'vt';$letter = 'u';
+	} elseif($title == 'Washington') {$sTitle = 'Washington'; $size = 'large-count'; $dataState = 'wa';$letter = 'v';
+	} elseif($title == 'West Virginia') {$sTitle = 'West Virginia'; $size = 'large-count'; $dataState = 'wv';$letter = 'w';
+	} elseif($title == 'Wisconson') {$sTitle = 'Wisconson'; $size = 'large-count'; $dataState = 'wi';$letter = 'x';
+	} elseif($title == 'Wyoming') {$sTitle = 'Wyoming'; $size = 'large-count'; $dataState = 'wy';$letter = 'y';
 	} 
 
     // State color and render
@@ -99,7 +103,9 @@ get_header(); ?>
 	array_push($numArray, array(
 		'desc' => $desc,
 		'num' => $numProp,
-		'datastate' => $dataState
+		'datastate' => $dataState,
+		'size' => $size, 
+		'title' => $sTitle
     ));
 
 
@@ -130,10 +136,16 @@ get_header(); ?>
 					
 					echo '<div class="' . $stateClass . ' ' . $numDesc['datastate'] . '">';
 
-					if( $myNum !== '' ) {
-						echo '<div class="count wow zoomIn">' . $myNum . '</div>';
-					}
-					echo '<div id="js-target" class="displaynone ">' . $numDesc['desc'] . '</div>';
+						echo '<div class="state-name">'. $numDesc['title'] . '</div>';
+
+						if( $myNum !== '' ) {
+							echo '<div class="count ' . $numDesc['size'] . ' wow zoomIn">' . $myNum . '</div>';
+						}
+
+						// Show hover only if we have something...
+						if( $numDesc['desc'] != '') {
+							echo '<div id="js-target" class="displaynone ">' . $numDesc['desc'] . '</div>';
+						}
 
 					echo '</div>'; // close it so it's a child
 				}	
@@ -150,7 +162,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+</div><!-- wrapper -->
 <?php
-get_sidebar();
+
 get_footer();
