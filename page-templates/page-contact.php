642394ba9b6639a83ed7fe1119bd9686be6159bc
Austin Crane <austincrane@gmail.com>
2016-05-03 10:06:57 -0400

    contact page

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -5,24 +5,46 @@
 
 get_header(); ?>
 <div class="wrapper">
-	<div id="primary" class="content-area">
+	<div id="primary" class="contact-area">
 		<main id="main" class="site-main" role="main">
 
 			<?php
-			while ( have_posts() ) : the_post();
+			while ( have_posts() ) : the_post(); 
 
-				get_template_part( 'template-parts/content', 'page' );
+			$image = get_field('picture');
+			$content = get_field('content');
+			$map = get_field('map');
+			$size = 'large';
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
 
-			endwhile; // End of the loop.
+			?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
+
+				<?php 
+					if( $image ) {
+						echo wp_get_attachment_image( $image, $size );
+					} 
+					?>
+
+				<div class="entry-content">
+					<?php echo $content; ?>
+				</div><!-- .entry-content -->
+
+			</article><!-- #post-## -->
+			<?php endwhile; // End of the loop.
 			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
+
+	<div class="map-area">
+		<?php echo $map; ?>
+	</div>
+
 </div>
 <?php
 get_footer();
